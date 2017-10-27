3e5f4790193297e93d4d393ce72a1c83bcbb6f5a
EurekaRobotics <rreynolds@teameureka.net>
2017-10-27 09:18:31 -0600

    Spin added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleCode/ConceptHolonomicAutoDriveByTime.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleCode/ConceptHolonomicAutoDriveByTime.java
@@ -1,46 +1,4 @@
 /*
-Copyright (c) 2016 Robert Atkinson
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted (subject to the limitations in the disclaimer below) provided that
-the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list
-of conditions and the following disclaimer.
-
-Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-Neither the name of Robert Atkinson nor the names of his contributors may be used to
-endorse or promote products derived from this software without specific prior
-written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-package org.firstinspires.ftc.teamcode.ExampleCode;
-
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.teamcode.ExampleCode.HardwareSetupHolonomicExample;
-
-/*
    Holonomic/Mecanum concept autonomous program. Driving motors for TIME
 
    Robot wheel mapping:
@@ -54,6 +12,15 @@ import org.firstinspires.ftc.teamcode.ExampleCode.HardwareSetupHolonomicExample;
         X           X
           X       X
 */
+package org.firstinspires.ftc.teamcode.ExampleCode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.ExampleCode.HardwareSetupHolonomicExample;
 
 @Autonomous(name="Concept Holonomic AutoByTime", group="Test")
 //@Disabled
@@ -95,6 +62,7 @@ public class ConceptHolonomicAutoDriveByTime extends LinearOpMode {
 /* currently no Servo configured on bot
         RaiseArm();
 */
+        SpinRight(DRIVE_POWER/2, 3000);
         DriveForwardTime(DRIVE_POWER, 4000);
         StopDriving();
 
@@ -108,7 +76,7 @@ public class ConceptHolonomicAutoDriveByTime extends LinearOpMode {
     public void DriveForward(double power)
     {
         // write the values to the motors
-        robot.motorFrontRight.setPower(DRIVE_POWER);
+        robot.motorFrontRight.setPower(DRIVE_POWER);//still need to test motor directions for desired movement
         robot.motorFrontLeft.setPower(-DRIVE_POWER);
         robot.motorBackRight.setPower(DRIVE_POWER);
         robot.motorBackLeft.setPower(-DRIVE_POWER);
@@ -134,8 +102,8 @@ public class ConceptHolonomicAutoDriveByTime extends LinearOpMode {
     {
         // write the values to the motors
         robot.motorFrontRight.setPower(-power);
-        robot.motorFrontLeft.setPower(power);
-        robot.motorBackRight.setPower(-power);
+        robot.motorFrontLeft.setPower(-power);
+        robot.motorBackRight.setPower(power);
         robot.motorBackLeft.setPower(power);
         Thread.sleep(time);
     }
@@ -144,6 +112,22 @@ public class ConceptHolonomicAutoDriveByTime extends LinearOpMode {
     {
         StrafeLeft(-power, time);
     }
+
+    public void SpinRight (double power, long time) throws InterruptedException
+    {
+        // write the values to the motors
+        robot.motorFrontRight.setPower(-power);
+        robot.motorFrontLeft.setPower(power);
+        robot.motorBackRight.setPower(-power);
+        robot.motorBackLeft.setPower(power);
+        Thread.sleep(time);
+    }
+
+    public void SpinLeft (double power, long time) throws InterruptedException
+    {
+        SpinRight(-power, time);
+    }
+
 
 /*** Currently no Servo configured in Holonomic Hardware setup
 
